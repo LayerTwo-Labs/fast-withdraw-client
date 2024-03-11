9c9ef6ccf53814aa168709d0445080c7e23caf22
CryptAxe <cryptaxe@gmail.com>
2024-03-11 16:46:34 -0700

    Add default export paths

--- client.gd
+++ client.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 	print("Client started. Connected to ID: ", peer.get_unique_id())
 	print("Is it a server?: ", multiplayer.is_server())
 
+
 func _on_button_test_pressed() -> void:
 	# Send fast withdraw request only to server
 	$"/root/Net".request_fast_withdraw.rpc_id(1, 21.0)
