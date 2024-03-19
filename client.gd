f2d086726f3bfdb3c4adea76e4800a49e815ab7f
CryptAxe <cryptaxe@gmail.com>
2024-03-18 21:25:55 -0700

    Test fast withdraw request

--- client.gd
+++ client.gd
@@ -8,11 +8,41 @@ func _ready() -> void:
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_client(SERVER_1, PORT)
 	multiplayer.multiplayer_peer = peer
+	
+	$"/root/Net".fast_withdraw_invoice.connect(_on_fast_withdraw_invoice)
+	$"/root/Net".fast_withdraw_complete.connect(_on_fast_withdraw_complete)
 
-	print("Client started. Connected to ID: ", peer.get_unique_id())
-	print("Is it a server?: ", multiplayer.is_server())
+	print("Client started. Peer ID: ", peer.get_unique_id())
 
 
 func _on_button_test_pressed() -> void:
 	# Send fast withdraw request only to server
-	$"/root/Net".request_fast_withdraw.rpc_id(1, 21.0)
+	$"/root/Net".request_fast_withdraw.rpc_id(1, $SpinBoxAmount.value, $LineEditMainchainAddress.text)
+
+
+func _on_button_test_complete_pressed() -> void:
+	# Tell the server we paid
+	var txid : String = $LineEditTXID.text
+	$"/root/Net".invoice_paid.rpc_id(1, txid, $SpinBoxAmount.value, $LineEditMainchainAddress.text)
+
+
+func _on_fast_withdraw_invoice(amount : float, destination: String) -> void:
+	print("Received fast withdraw invoice!")
+	print("Amount: ", amount)
+	print("Destination: ", destination)
+	
+	var invoice_text = "Fast withdraw request received!\n"
+	invoice_text += str("Send ", amount, " BTC to ", destination, "\n") 
+	invoice_text += "Then enter the L2 txid and hit invoice paid"
+	
+	$LabelInvoice.text = invoice_text
+
+
+func _on_fast_withdraw_complete(txid: String, amount : float, destination: String) -> void:
+	print("Fast withdraw complete!")
+	print("TxID: ", txid)
+	print("Amount: ", amount)
+	print("Destination: ", destination)
+	
+	$LabelComplete.text = str("Withdraw complete: ", txid)
+
--- net.gd
+++ net.gd
@@ -1,14 +1,50 @@
 extends Node
 
 # Server signals
-signal fast_withdraw_requested(amount: float)
+signal fast_withdraw_requested(peer : int, amount: float, destination: String)
+signal fast_withdraw_invoice_paid(peer : int, txid: String, amount: float, destination: String)
 
 # Client signals
+signal fast_withdraw_invoice(amount: float, destination: String)
+signal fast_withdraw_complete(txid: String, amount: float, destination: String)
 
 # TODO add params: SC #, MC fee, MC destination
 @rpc("any_peer", "call_remote", "reliable")
-func request_fast_withdraw(amount : float) -> void:
+func request_fast_withdraw(amount : float, destination : String) -> void:
 	print("Received fast withdrawal request")
 	print("Amount: ", amount)
+	print("Destination: ", destination)
+	print("Peer: ", multiplayer.get_remote_sender_id())
 	
-	fast_withdraw_requested.emit(amount)
+	fast_withdraw_requested.emit(multiplayer.get_remote_sender_id(), amount, destination)
+
+
+@rpc("authority", "call_remote", "reliable")
+func receive_fast_withdraw_invoice(amount : float, destination : String) -> void:
+	print("Received fast withdrawal invoice")
+	print("Amount: ", amount)
+	print("Destination: ", destination)
+	print("Peer: ", multiplayer.get_remote_sender_id())
+	
+	fast_withdraw_invoice.emit(amount, destination)
+
+
+@rpc("any_peer", "call_remote", "reliable")
+func invoice_paid(txid: String, amount : float, destination : String) -> void:
+	print("Paid fast withdrawal invoice")
+	print("Amount: ", amount)
+	print("Destination: ", destination)
+	print("Txid: ", txid)
+	print("Peer: ", multiplayer.get_remote_sender_id())
+	
+	fast_withdraw_invoice_paid.emit(multiplayer.get_remote_sender_id(), txid, amount, destination)
+
+
+@rpc("authority", "call_remote", "reliable")
+func withdraw_complete(txid: String, amount : float, destination : String) -> void:
+	print("Fast withdraw completed!")
+	print("Amount: ", amount)
+	print("Destination: ", destination)
+	print("Txid: ", txid)
+	
+	fast_withdraw_complete.emit(txid, amount, destination)
